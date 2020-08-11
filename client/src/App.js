b46b0d87dfb5ef07fe60b6ab16bfdd1e8cafbb0a
Sharon Kim <sharonkim1709@gmail.com>
2020-08-10 21:15:23 -0400

    created navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import NavBar from "./components/NavBar/NavBar";
 
-// import './App.css';
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
-    <div>
-    <h1>Beginning</h1>
+    <div id="body">
+
+
+    <Router>
+      <NavBar/>
+    </Router>
     </div>
   );
 }
---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <div className="navbar-fixed">
+      <nav className="black">
+        <div className="container">
+          <div className="nav-wrapper">
+            <ul className="d-flex justify-content-center col-6">
+              <li>
+                <Link to="/">Google Search</Link>
+              </li>
+              <li>
+                <Link to="/search">Search</Link>
+              </li>
+              <Link to="/saved">Saved</Link>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
