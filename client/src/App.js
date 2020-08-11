78b38847981a022bf1edf5975510f7b7bd1e370d
Sharon Kim <sharonkim1709@gmail.com>
2020-08-10 21:20:11 -0400

    added header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React from "react";
 import NavBar from "./components/NavBar/NavBar";
-
+import Header from "./components/Header/Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
     <div id="body">
-
-
-    <Router>
-      <NavBar/>
-    </Router>
+      <Router>
+        <NavBar />
+        <Header/>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+const Header = () => {
+  return (
+      <div id="headerEl">
+          <div className="center">
+          <h1>Google Books Search</h1>
+        <h5>Search for and Save Book of Interest</h5>
+          </div>
+      </div>
+  );
+};
+
+export default Header;
